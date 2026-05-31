fc4a6e2da6ebfde73bfd80f8810860dd8c57a0a5
Yasir Bilgin <yasirbil@gmail.com>
2026-05-31 13:17:45 -0500

    afaq-enfus wisdom formation

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    visits and works offline for previously visited pages.
    ============================================================ */
 
-const CACHE = 'ynb-v7';
+const CACHE = 'ynb-v8';
 const SHELL = [
   '/',
   '/index.html',
