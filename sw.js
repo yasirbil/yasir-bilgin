3c5af420a268d76d72895d4464c50a1224010f56
Yasir Bilgin <yasirbil@gmail.com>
2026-06-10 22:58:45 -0500

    reality wisdom

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    visits and works offline for previously visited pages.
    ============================================================ */
 
-const CACHE = 'ynb-v17';
+const CACHE = 'ynb-v18';
 const SHELL = [
   '/',
   '/index.html',
