789acbd17d62db8ff7b6b6eb45f8903ccf025800
Yasir Bilgin <yasirbil@gmail.com>
2026-06-11 19:23:35 -0500

    europe history

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    visits and works offline for previously visited pages.
    ============================================================ */
 
-const CACHE = 'ynb-v24';
+const CACHE = 'ynb-v25';
 const SHELL = [
   '/',
   '/index.html',
