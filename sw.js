c6e20ef9afa5be2d6099597bb39756a8470a9a67
Yasir Bilgin <yasirbil@gmail.com>
2026-06-12 20:27:09 -0500

    fixed family therapy

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    visits and works offline for previously visited pages.
    ============================================================ */
 
-const CACHE = 'ynb-v34';
+const CACHE = 'ynb-v35';
 const SHELL = [
   '/',
   '/index.html',
