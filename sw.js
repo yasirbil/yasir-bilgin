701fb39d3237560484571bed36786673b820b119
Yasir Bilgin <yasirbil@gmail.com>
2026-07-05 01:47:20 -0500

    inner observability dashboard

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    visits and works offline for previously visited pages.
    ============================================================ */
 
-const CACHE = 'ynb-v41';
+const CACHE = 'ynb-v42';
 const SHELL = [
   '/',
   '/index.html',
