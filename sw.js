2858a36a39597601820e543d3bbfedc2b6e75f11
Yasir Bilgin <yasirbil@gmail.com>
2026-07-15 23:37:12 -0500

    which sure for which condition

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    visits and works offline for previously visited pages.
    ============================================================ */
 
-const CACHE = 'ynb-v45';
+const CACHE = 'ynb-v46';
 const SHELL = [
   '/',
   '/index.html',
