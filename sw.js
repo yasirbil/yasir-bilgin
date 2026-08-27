61f3cfa0f42d2ecfe6bdf71deb005d36f285a423
Yasir Bilgin <yasirbil@gmail.com>
2026-08-26 21:45:08 -0500

    polity atlas

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    visits and works offline for previously visited pages.
    ============================================================ */
 
-const CACHE = 'ynb-v60';
+const CACHE = 'ynb-v61';
 const SHELL = [
   '/',
   '/index.html',
