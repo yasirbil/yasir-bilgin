9bbd05c48b30a327e86b8e320626f4f59559e4c5
Yasir Bilgin <yasirbil@gmail.com>
2026-09-11 22:37:41 -0500

    experience

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    visits and works offline for previously visited pages.
    ============================================================ */
 
-const CACHE = 'ynb-v63';
+const CACHE = 'ynb-v64';
 const SHELL = [
   '/',
   '/index.html',
