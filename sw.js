02b3809f44cf29893fa1fea357e73703a16257e8
Yasir Bilgin <yasirbil@gmail.com>
2026-09-13 21:02:22 -0500

    hane-i saadette ihlas

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    visits and works offline for previously visited pages.
    ============================================================ */
 
-const CACHE = 'ynb-v67';
+const CACHE = 'ynb-v68';
 const SHELL = [
   '/',
   '/index.html',
